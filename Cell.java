227ccf4d572d0ceb085863b97b45c7e4e84cfb82
ChloeBallantyne <160100291+ChloeBallantyne@users.noreply.github.com>
2024-05-02 12:57:10 +1000

    Added Cell class

---
+++ Cell.java
@@ -0,0 +1,35 @@
+public class Cell {
+    private boolean mine;
+    private boolean revealed;
+    private int adjacentMines;
+
+    public Cell() {
+        mine = false;
+        revealed = false;
+        adjacentMines = 0;
+    }
+
+    public boolean isMine() {
+        return mine;
+    }
+
+    public void setMine(boolean mine) {
+        this.mine = mine;
+    }
+
+    public boolean isRevealed() {
+        return revealed;
+    }
+
+    public void setRevealed(boolean revealed) {
+        this.revealed = revealed;
+    }
+
+    public int getAdjacentMines() {
+        return adjacentMines;
+    }
+
+    public void setAdjacentMines(int adjacentMines) {
+        this.adjacentMines = adjacentMines;
+    }
+}
